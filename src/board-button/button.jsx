3eb48a97a6e6601449b3e567d3d01beca9811e2f
habib-technonext <habibur.rahman@technonext.com>
2024-10-24 15:37:43 +0600

    onClick is added in the button

--- src/board-button/button.jsx
+++ src/board-button/button.jsx
@@ -1,7 +1,13 @@
 const Square = ({ value }) => {
+  const handleClick = () => {
+    console.log("Button is clickced!");
+  };
   return (
     <>
-      <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg">
+      <button
+        onClick={handleClick}
+        className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg"
+      >
         {value}
       </button>
     </>
