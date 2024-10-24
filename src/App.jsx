6327e19618ef292287df39903b740f779f61a2bf
habib-technonext <habibur.rahman@technonext.com>
2024-10-24 15:23:52 +0600

    button component is created

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,21 @@ function App() {
 
       <div className="text-center">
         <div>
-          <Square />
-          <Square />
-          <Square />
+          <Square value="1" />
+          <Square value="2" />
+          <Square value="3" />
         </div>
 
         <div>
-          <Square />
-          <Square />
-          <Square />
+          <Square value="4" />
+          <Square value="5" />
+          <Square value="6" />
         </div>
 
         <div>
-          <Square />
-          <Square />
-          <Square />
+          <Square value="7" />
+          <Square value="8" />
+          <Square value="9" />
         </div>
       </div>
     </>
--- src/board-button/button.jsx
+++ src/board-button/button.jsx
@@ -1,8 +1,8 @@
-const Square = () => {
+const Square = ({ value }) => {
   return (
     <>
       <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg">
-        X
+        {value}
       </button>
     </>
   );
